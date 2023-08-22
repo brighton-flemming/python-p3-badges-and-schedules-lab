8068cd21f8e8549ba04ac6fe2679c4e1311b836a
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-22 11:31:18 +0300

    Mission Passed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,8 +1,22 @@
 def badge_maker(name):
-    return None
+    greeting = f'Hello, my name is {name}.'
+    print(greeting)
+    return greeting
 
 def batch_badge_creator(names):
-    return None
+ badges=[]
+ for name in names:
+   message = badge_maker(name)
+   badges.append(message)
+ return badges
+   
+name_list = ["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace"]
+badges_messages =  batch_badge_creator(name_list)
+
+for message in badges_messages:
+    print(message)
+
+   
 
 def assign_rooms(names):
     return None
