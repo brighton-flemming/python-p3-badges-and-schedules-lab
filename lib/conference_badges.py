5d579f3054f4b270cec52f0ea0a704887048cea3
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-22 12:02:18 +0300

    Mission Passed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -10,16 +10,36 @@ def batch_badge_creator(names):
    badges.append(message)
  return badges
    
-name_list = ["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace"]
+name_list = ["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace", "Matz"]
 badges_messages =  batch_badge_creator(name_list)
 
 for message in badges_messages:
     print(message)
 
-   
 
 def assign_rooms(names):
-    return None
+ list = []
+ 
+ for index,name in enumerate (names):
+    if index < len(room_num):
+     welcoming = f"Hello, {name}! You'll be assigned to room {room_num[index]}!"
+     list.append(welcoming)
+    else:
+       welcoming = f"Hello {name}! Room number is not available!"
+       print(welcoming)
+
+ return list
+
+room_num = [1, 2, 3, 4 , 5, 6, 7, 8]
+name_list = ["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace","Frankie", "Matz" ]
+list = assign_rooms(name_list)
+
+for welcoming in list:
+    print (welcoming)
+ 
+
+
+
 
 def printer(names):
     return None
